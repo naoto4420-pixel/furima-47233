ec4ac7eacc8bc158678c74a36742846270b67a8c
naoto4420-pixel <naoto4420@gmail.com>
2026-01-14 17:45:21 +0900

    商品データ保存設定
    createアクションの設定、newビューファイルの調整。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,21 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # プライベートメソッド
+  private
+
+  # ストロングパラメーターパラメーター設定
+  def item_params
+    params.require(:item).permit(:image, :name, :explain, :price, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :estimated_shipping_date_id).merge(user_id: :current_user.id)
+
+
 end

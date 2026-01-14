404161b44c981505a218997921b174fc91c4b789
naoto4420-pixel <naoto4420@gmail.com>
2026-01-14 19:03:16 +0900

    itemモデルのバリデーション設定調整2

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,13 +12,23 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   # バリデーション
+  validates :image,                       presence: true
   validates :name,                        presence: true
   validates :explain,                     presence: true
-  validates :price,                       presence: { message: "can't be blank"}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "value out of range" }
-  validates :category_id,                 presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id,                presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_cost_id,            presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id,               presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :estimated_shipping_date_id,  presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,                       presence: { message: "can't be blank"}
+  validates :price,                       numericality: { only_integer: true, message: "using half-width digits only" }, allow_blank: true
+  with_options if: -> { errors[:price].blank? } do |v|
+    v.validates :price,                       numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "value out of range" }
+  end
+  validates :category_id,                 presence: true, 
+                                          numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,                presence: true, 
+                                          numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id,            presence: true, 
+                                          numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,               presence: true, 
+                                          numericality: { other_than: 1, message: "can't be blank" }
+  validates :estimated_shipping_date_id,  presence: true, 
+                                          numericality: { other_than: 1, message: "can't be blank" }
 
 end

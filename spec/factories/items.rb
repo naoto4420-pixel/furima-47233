fe3c16e4ca4252f8c451a134fe6fc7d0ccd6a81a
naoto4420-pixel <naoto4420@gmail.com>
2026-01-15 14:18:46 +0900

    商品テストデータ設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name                          { Faker::Name.title }
+    explain                       { Faker::Lorem.sentence }
+    price                         { Faker::Number.between(from: 300, to: 9999999) }
+    category_id                   { Faker::Number.between(from: 2, to: 11) }
+    condition_id                  { Faker::Number.between(from: 2, to: 7) }
+    shipping_cost_id              { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id                 { Faker::Number.between(from: 2, to: 48) }
+    estimated_shipping_date_id    { Faker::Number.between(from: 2, to: 4) }
+
+    association :user
+
+    # テスト画像
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/test_pic.png'), filename: 'test_pic.png', content_type: 'image/png')
+    end
+
   end
 end
